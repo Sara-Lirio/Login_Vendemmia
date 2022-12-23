cd2ca93225f6c30af8e9b14b07507d28348a2ccd
Sara <saraeliza.lirio@gmail.com>
2022-12-23 03:07:51 -0300

    Fazendo requisição da API

---
+++ src/service/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://63a1c51eba35b96522e7a1b1.mockapi.io/vdm'
+})
+
+export async function getUsers() {
+    const response = await api.get('/Users');
+    console.log(response.data);
+    return response.data
+}
+
+getUsers()
