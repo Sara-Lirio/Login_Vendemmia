b88ec028b3c9578e11d893af9350fdad940f3767
Sara <saraeliza.lirio@gmail.com>
2022-12-23 00:39:09 -0300

    Adicionando props no componente Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import style from './Button.module.css'
 
-const Button = () => {
+const Button = ({ type, onClick, text, disabled}) => {
   return (
-    <button className={style.btn}>Entrar</button>
+    <button className={style.btn}
+    type={type}
+    onClick={onClick} disabled={disabled}
+    >{text}</button>
   )
 }
 
